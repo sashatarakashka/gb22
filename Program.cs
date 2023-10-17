8390820d01f40a20892aeb647d4df56f7d2df796
sashkatarakashka <lapakoza@gmail.com>
2023-10-17 12:01:07 +0300

    Решение задачи №10

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int x = Convert.ToInt32(Console.ReadLine());
+if ((x<100)||(x>999))
+{
+    Console.WriteLine("Число не трехзначное");
+}
+else
+{
+  int a = x/10;
+int b = a%10;
+Console.WriteLine(b);  
+}
+
